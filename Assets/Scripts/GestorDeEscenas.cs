0b1c4d9752275aa08645e9e64c04abd92d13f062
IsmaelPerezRodriguez (UNI) <ismaeluniperez@gmail.com>
2025-01-24 21:28:50 +0100

    -Añadido el script con cambio de escena -Añadido el menú de pausa casi funcional -Corregida la cámara del tanque del juego

--- Assets/Scripts/GestorDeEscenas.cs
+++ Assets/Scripts/GestorDeEscenas.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GestorDeEscenas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void CargarEscena()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(1);
     }
 }
